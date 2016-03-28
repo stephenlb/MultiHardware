0c670cd83497ff28b02f9f00b238dee725ebeffb
Bhavana Srinivas <bana.srini@gmail.com>
2016-03-28 16:34:35 -0700

    Create the file
    Publishes humidity and temperature values from the Pi

---
+++ motortemp.py
@@ -0,0 +1,30 @@
+import os
+import time
+import sys
+from pubnub import Pubnub
+import Adafruit_DHT as dht
+
+
+pubnub = Pubnub(publish_key='pub-c-6dbe7bfd-6408-430a-add4-85cdfe856b47', subscribe_key='sub-c-2a73818c-d2d3-11e3-9244-02ee2ddab7fe')
+ #use your own keys
+ #need to add auth-key
+
+def callback(message):
+    print(message)
+
+#published in this fashion to comply with Eon
+while True:
+    h,t = dht.read_retry(dht.DHT22, 4)
+    pubnub.publish('tempeon', {
+        'columns': [
+            ['temp-pi', t],
+            ['username', MotorTemp]
+            ]
+        })
+    pubnub.publish('humeon', {
+        'columns': [
+            ['humidity', h],
+            ['username', Humidity]
+            ]
+
+        })
