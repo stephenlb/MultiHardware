d1f25296f36d4f85df85cc70d31df786cfe7c176
Bhavana Srinivas <bana.srini@gmail.com>
2016-03-29 15:26:30 -0700

    Update motortemp.py

--- motortemp.py
+++ motortemp.py
@@ -7,7 +7,7 @@ from pubnub import Pubnub
 import Adafruit_DHT as dht
 
 
-pubnub = Pubnub(publish_key='pub-c-6dbe7bfd-6408-430a-add4-85cdfe856b47', subscribe_key='sub-c-2a73818c-d2d3-11e3-9244-02ee2ddab7fe')
+pubnub = Pubnub(publish_key='pub-c-6dbe7bfd-6408-430a-add4-85cdfe856b47', subscribe_key='sub-c-2a73818c-d2d3-11e3-9244-02ee2ddab7fe', auth_key ='device_security1234')
  #use your own keys
  #need to add auth-key
 
